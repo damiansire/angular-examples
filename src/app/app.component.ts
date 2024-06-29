a62410fcf47d5de0d3e8c033b18868c584c07039
Damian Sire <damiansire@gmail.com>
2024-06-28 23:09:26 -0300

    feat: add basic example for writable-signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WritableSignalsComponent } from './signals/writable-signals/writable-signals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, WritableSignalsComponent],
 })
 export class AppComponent {
   title = 'angular-examples';
--- src/app/signals/writable-signals/writable-signals.component.ts
+++ src/app/signals/writable-signals/writable-signals.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-writable-signals',
   standalone: true,
   imports: [],
   templateUrl: './writable-signals.component.html',
-  styleUrl: './writable-signals.component.css'
+  styleUrl: './writable-signals.component.css',
 })
 export class WritableSignalsComponent {
-
+  count = signal(0);
+  ngOnInit() {
+    // Signals are getter functions - calling them reads their value.
+    console.log('The count is: ' + this.count());
+    this.count.set(3);
+    // Increment the count by 1.
+  }
+  update() {
+    this.count.update((value) => value + 1);
+    console.log(this.count());
+  }
 }
