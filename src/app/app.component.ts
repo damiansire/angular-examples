6557afdf783a1346693fc5f3788d613383c8c3c3
Damian Sire <damiansire@gmail.com>
2024-06-29 19:54:06 -0300

    feat(signals-3): add new component for show signals lazily

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WritableSignalsComponent } from './signals/1-writable-signals/writable-signals.component';
 import { ComputedSignalsComponent } from './signals/2-computed-signals/computed-signals.component';
+import { ComputedSignalsLazilyEvaluatedMemoizedComponent } from './signals/3-computed-signals-lazily-evaluated-memoized/computed-signals-lazily-evaluated-memoized.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, WritableSignalsComponent, ComputedSignalsComponent],
+  imports: [
+    RouterOutlet,
+    WritableSignalsComponent,
+    ComputedSignalsComponent,
+    ComputedSignalsLazilyEvaluatedMemoizedComponent,
+  ],
 })
 export class AppComponent {
   title = 'angular-examples';
--- src/app/signals/3-computed-signals-lazily-evaluated-memoized/computed-signals-lazily-evaluated-memoized.component.ts
+++ src/app/signals/3-computed-signals-lazily-evaluated-memoized/computed-signals-lazily-evaluated-memoized.component.ts
@@ -1,20 +1,71 @@
 import {
   Component,
+  ElementRef,
+  Input,
   Signal,
+  ViewChild,
   WritableSignal,
   computed,
   signal,
 } from '@angular/core';
 
 @Component({
-  selector: 'app-computed-signals-lazily-evaluated-memoized',
+  selector: 'app-profile-card',
   standalone: true,
-  imports: [],
-  templateUrl: './computed-signals-lazily-evaluated-memoized.component.html',
-  styleUrl: './computed-signals-lazily-evaluated-memoized.component.css',
+  template: `
+    <div class="profile-container">
+      <h1>{{ title }}</h1>
+      <div class="profile-info">
+        <label for="firstName">Nombre:</label>
+        <input type="text" placeholder="Name" #firstNameInput />
+        <br />
+        <label for="lastName">Apellido:</label>
+        <input type="text" placeholder="Surname" #lastNameInput />
+        <br />
+        <button class="btn btn-primary" (click)="setValue()">Set</button>
+      </div>
+      <div class="profile-greeting">
+        <p>Hola, {{ fullName() }}!</p>
+      </div>
+    </div>
+  `,
+  styles: `.profile-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+.profile-info {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 15px;
+}
+
+label {
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+input {
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+}
+
+.profile-greeting {
+    font-size: 1.2em;
+}`,
+  styleUrls: [],
 })
-export class ComputedSignalsLazilyEvaluatedMemoizedComponent {
+export class ProfileCardComponent {
+  @Input() title = 'Persona';
+  @ViewChild('firstNameInput') firstNameInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('lastNameInput') lastNameInput!: ElementRef<HTMLInputElement>;
   computedTracker: Date[] = [];
+
   firstName: WritableSignal<string> = signal('Damian');
   lastName: WritableSignal<string> = signal('Sire');
   fullName: Signal<string> = computed(() => {
@@ -22,13 +73,21 @@ export class ComputedSignalsLazilyEvaluatedMemoizedComponent {
     return `${this.firstName()} ${this.lastName()}`;
   });
 
-  setFirstName(eventTarget: any) {
-    const value = eventTarget?.value || '';
-    this.firstName.set(value);
+  setValue() {
+    const firstName = this.firstNameInput.nativeElement.value;
+    const lastName = this.lastNameInput.nativeElement.value;
+    this.lastName.set(firstName);
+    this.firstName.set(lastName);
   }
+}
 
-  setLastName(eventTarget: any) {
-    const value = eventTarget?.value || '';
-    this.lastName.set(value);
-  }
+@Component({
+  selector: 'app-computed-signals-lazily-evaluated-memoized',
+  standalone: true,
+  imports: [ProfileCardComponent],
+  templateUrl: './computed-signals-lazily-evaluated-memoized.component.html',
+  styleUrl: './computed-signals-lazily-evaluated-memoized.component.css',
+})
+export class ComputedSignalsLazilyEvaluatedMemoizedComponent {
+  computedTracker: Date[] = [];
 }
