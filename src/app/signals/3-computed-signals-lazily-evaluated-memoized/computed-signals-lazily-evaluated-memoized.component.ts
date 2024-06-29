837e1d8b41222fdf36b1b8ca9afa6067397b2562
Damian Sire <damiansire@gmail.com>
2024-06-29 16:12:47 -0300

    refactor: change order of components and add news

--- src/app/signals/3-computed-signals-lazily-evaluated-memoized/computed-signals-lazily-evaluated-memoized.component.ts
+++ src/app/signals/3-computed-signals-lazily-evaluated-memoized/computed-signals-lazily-evaluated-memoized.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import {
   Component,
   Signal,
@@ -6,19 +5,20 @@ import {
   computed,
   signal,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-computed-signals',
+  selector: 'app-computed-signals-lazily-evaluated-memoized',
   standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './computed-signals.component.html',
-  styleUrl: './computed-signals.component.css',
+  imports: [],
+  templateUrl: './computed-signals-lazily-evaluated-memoized.component.html',
+  styleUrl: './computed-signals-lazily-evaluated-memoized.component.css',
 })
-export class ComputedSignalsComponent {
+export class ComputedSignalsLazilyEvaluatedMemoizedComponent {
+  computedTracker: Date[] = [];
   firstName: WritableSignal<string> = signal('Damian');
   lastName: WritableSignal<string> = signal('Sire');
   fullName: Signal<string> = computed(() => {
+    this.computedTracker.push(new Date());
     return `${this.firstName()} ${this.lastName()}`;
   });
 
