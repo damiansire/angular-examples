6462da399bc50e2536e07a1c36787496ae13ec87
Damian Sire <damiansire@gmail.com>
2024-06-29 02:32:50 -0300

    feat: add example of computed-signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WritableSignalsComponent } from './signals/1-writable-signals/writable-signals.component';
+import { ComputedSignalsComponent } from './signals/2-computed-signals/computed-signals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, WritableSignalsComponent],
+  imports: [RouterOutlet, WritableSignalsComponent, ComputedSignalsComponent],
 })
 export class AppComponent {
   title = 'angular-examples';
--- src/app/signals/2-computed-signals/computed-signals.component.ts
+++ src/app/signals/2-computed-signals/computed-signals.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  Signal,
+  WritableSignal,
+  computed,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-computed-signals',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './computed-signals.component.html',
-  styleUrl: './computed-signals.component.css'
+  styleUrl: './computed-signals.component.css',
 })
 export class ComputedSignalsComponent {
+  firstName: WritableSignal<string> = signal('Damian');
+  lastName: WritableSignal<string> = signal('Sire');
+  fullName: Signal<string> = computed(
+    () => `${this.firstName()} ${this.lastName()}`
+  );
 
+  setFirstName(eventTarget: any) {
+    const value = eventTarget?.value || '';
+    this.firstName.set(value);
+  }
+
+  setLastName(eventTarget: any) {
+    const value = eventTarget?.value || '';
+    this.lastName.set(value);
+  }
 }
