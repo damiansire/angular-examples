0bb58dc400ca66261be54f651a2a581ddfb76399
Damian Sire <damiansire@gmail.com>
2024-06-30 01:50:19 -0300

    feat: create structure for example computed-signals-lazily-evaluated-memoized

---
+++ src/app/signals/computed-signals-lazily-evaluated-memoized/computed-signals-lazily-evaluated-memoized.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-computed-signals-lazily-evaluated-memoized',
+  standalone: true,
+  imports: [],
+  templateUrl: './computed-signals-lazily-evaluated-memoized.component.html',
+  styleUrl: './computed-signals-lazily-evaluated-memoized.component.css'
+})
+export class ComputedSignalsLazilyEvaluatedMemoizedComponent {
+
+}
