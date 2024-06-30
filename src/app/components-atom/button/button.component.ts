5d80665e7d7f769f47b7053fd735c0c30913d451
Damian Sire <damiansire@gmail.com>
2024-06-30 05:44:45 -0300

    feat: add button component

--- src/app/components-atom/button/button.component.ts
+++ src/app/components-atom/button/button.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
 })
 export class ButtonComponent {
+  @Output() onClick: any = new EventEmitter<void>();
+  @Input() text: string = 'Missing text';
 
+  callClick() {
+    this.onClick.emit('emitido');
+  }
 }
