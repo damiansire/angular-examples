7fe84607902ad600f7b4b2f3bd8b09906476d8a9
Damian Sire <damiansire@gmail.com>
2024-06-30 02:19:58 -0300

    feat: add basic form component logic

--- src/app/components/basic-form/basic-form.component.ts
+++ src/app/components/basic-form/basic-form.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  Signal,
+  ViewChild,
+  WritableSignal,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
+interface ClickInButton {
+  date: Date;
+  firstName: string;
+  surname: string;
+}
 
 @Component({
   selector: 'app-basic-form',
   standalone: true,
   imports: [],
   templateUrl: './basic-form.component.html',
-  styleUrl: './basic-form.component.css'
+  styleUrl: './basic-form.component.css',
 })
 export class BasicFormComponent {
+  firstName: WritableSignal<string> = signal('Damian');
+  surname: WritableSignal<string> = signal('Sire');
+  fullName: Signal<string> = computed(() => {
+    return `${this.firstName()} ${this.surname()}`;
+  });
+  @ViewChild('firstNameInput') firstNameInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('surnameInput') surnameInput!: ElementRef<HTMLInputElement>;
+  @Output() signalComputed = new EventEmitter<ClickInButton>();
+  @Output() buttonClicked = new EventEmitter<ClickInButton>();
 
+  constructor() {
+    effect(() => {
+      const currentFullName = this.fullName();
+      this.signalComputed.emit({
+        date: new Date(),
+        firstName: this.firstName(),
+        surname: this.surname(),
+      });
+    });
+  }
+
+  setValue() {
+    const firstName = this.firstNameInput.nativeElement.value;
+    const surname = this.surnameInput.nativeElement.value;
+    this.firstName.set(firstName);
+    this.surname.set(surname);
+    this.buttonClicked.emit({
+      date: new Date(),
+      firstName,
+      surname,
+    });
+  }
 }
