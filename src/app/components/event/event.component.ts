33ad013d7b01430c9993614f929749def9a23140
Damian Sire <damiansire@gmail.com>
2024-06-30 03:31:21 -0300

    design: new designs for events

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { ClickInButton } from '../component.interface';
 import { CommonModule } from '@angular/common';
+import { SvgComponent } from '../../../icons/click.component';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './event.component.html',
   styleUrl: './event.component.css',
+  imports: [CommonModule, SvgComponent],
 })
 export class EventComponent {
   @Input() event: ClickInButton = {
---
+++ src/icons/click.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'icon-click',
+  templateUrl: './click.component.svg',
+})
+export class SvgComponent {}
