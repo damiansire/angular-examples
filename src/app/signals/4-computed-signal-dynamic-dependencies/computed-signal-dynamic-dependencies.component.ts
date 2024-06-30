e0facb66f1d51b7e257a5e290b3767ecd0ac7106
Damian Sire <damiansire@gmail.com>
2024-06-30 05:29:39 -0300

    feat: add computed signals dynamic dependencies

--- src/app/signals/4-computed-signal-dynamic-dependencies/computed-signal-dynamic-dependencies.component.ts
+++ src/app/signals/4-computed-signal-dynamic-dependencies/computed-signal-dynamic-dependencies.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-computed-signal-dynamic-dependencies',
   standalone: true,
   imports: [],
   templateUrl: './computed-signal-dynamic-dependencies.component.html',
-  styleUrl: './computed-signal-dynamic-dependencies.component.css'
+  styleUrl: './computed-signal-dynamic-dependencies.component.css',
 })
 export class ComputedSignalDynamicDependenciesComponent {
-
+  showCount = signal(false);
+  count = signal(0);
+  conditionalCount = computed(() => {
+    if (this.showCount()) {
+      return `The count is ${count()}.`;
+    } else {
+      return 'Nothing to see here!';
+    }
+  });
+  upCount() {
+    this.count.update((currentCount: number) => currentCount + 1);
+  }
+  onShowCountChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const newValue = target.value === 'true';
+    this.showCount.set(newValue);
+  }
 }
