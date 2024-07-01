24a86159d0330012cf28ff83fa31609bf602dc8d
Damian Sire <damiansire@gmail.com>
2024-06-30 22:47:04 -0300

    feat: add logic for onClick

--- src/app/components-atom/variable-box/variable-box.component.ts
+++ src/app/components-atom/variable-box/variable-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-variable-box',
@@ -10,4 +10,10 @@ import { Component, Input } from '@angular/core';
 export class VariableBoxComponent {
   @Input() variableName: string = '';
   @Input() variableValue: string = '';
+
+  @Output() clickEvent = new EventEmitter<void>();
+
+  onClick() {
+    this.clickEvent.emit();
+  }
 }
