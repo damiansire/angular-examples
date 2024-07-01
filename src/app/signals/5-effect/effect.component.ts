6951d1692aa4ae9c8faae6b1aef4295cf519cbd9
Damian Sire <damiansire@gmail.com>
2024-07-01 08:11:45 -0300

    feat: add extra effect to effect example

--- src/app/signals/5-effect/effect.component.ts
+++ src/app/signals/5-effect/effect.component.ts
@@ -5,38 +5,71 @@ import { VariableBoxComponent } from '../../components-atom/variable-box/variabl
 import { HistoryElement } from '../../components/component.interface';
 import { EventHistoryComponent } from '../../components/event-history/event-history.component';
 import { trigger } from '@angular/animations';
+import { DependenciesStatusComponent } from '../../components/dependencies-status/dependencies-status.component';
 
 @Component({
   selector: 'app-effect',
   standalone: true,
   templateUrl: './effect.component.html',
   styleUrl: './effect.component.css',
-  imports: [CodeComponent, VariableBoxComponent, EventHistoryComponent],
+  imports: [
+    CodeComponent,
+    VariableBoxComponent,
+    EventHistoryComponent,
+    DependenciesStatusComponent,
+  ],
 })
 export class EffectComponent {
   count = signal(0);
+  count2 = signal(0);
+  dependencies = computed<string[]>(() => {
+    return ['count'];
+  });
+  dependencies2 = computed<string[]>(() => {
+    return ['count2'];
+  });
   appEventHistory = signal<HistoryElement[]>([]);
   constructor() {
     effect(
       () => {
         const trigger =
           this.count() === 0 ? 'Initial render' : 'Increment Count';
-        this.addConditionalCountRecomputation(trigger, 'Void', false);
+        this.addConditionalCountRecomputation(trigger, '1', false);
         console.log(`The current count is: ${this.count()}`);
+      },
+      { allowSignalWrites: true }
+    );
+    effect(
+      () => {
+        const trigger =
+          this.count2() === 0 ? 'Initial render' : 'Increment Count';
+        this.addConditionalCountRecomputation(trigger, '2', false);
+        console.log(`The current count is: ${this.count2()}`);
       },
       { allowSignalWrites: true }
     );
   }
   lines = computed<codeLine[]>(() => [
+    { line: 'count = signal(0);', active: false },
+    { line: 'count2 = signal(0);', active: false },
     { line: 'effect(() => {', active: false },
     {
       line: '  console.log(`The current count is: ${count()}`);',
       active: false,
     },
     { line: '});', active: false },
+    { line: 'effect(() => {', active: false },
+    {
+      line: '  console.log(`The current count is: ${count2()}`);',
+      active: false,
+    },
+    { line: '});', active: false },
   ]);
   upCount() {
     this.count.update((currentCount: number) => currentCount + 1);
+  }
+  upCount2() {
+    this.count2.update((currentCount: number) => currentCount + 1);
   }
   addConditionalCountRecomputation(
     trigger: string,
