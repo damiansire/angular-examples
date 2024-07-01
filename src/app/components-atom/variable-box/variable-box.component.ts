a7b59209755aecbc32f9595ad09060720a984857
Damian Sire <damiansire@gmail.com>
2024-06-30 22:34:28 -0300

    design: add design and logic to variable-box

--- src/app/components-atom/variable-box/variable-box.component.ts
+++ src/app/components-atom/variable-box/variable-box.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-variable-box',
   standalone: true,
   imports: [],
   templateUrl: './variable-box.component.html',
-  styleUrl: './variable-box.component.css'
+  styleUrl: './variable-box.component.css',
 })
 export class VariableBoxComponent {
-
+  @Input() variableName: string = '';
+  @Input() variableValue: string = '';
 }
