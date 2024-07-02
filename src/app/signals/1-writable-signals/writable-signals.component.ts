850460b7d3a9dc60f85ea918cd9d2771778d7d03
Damian Sire <damiansire@gmail.com>
2024-07-02 18:27:27 -0300

    feat: add code preview to writable signal

--- src/app/signals/1-writable-signals/writable-signals.component.ts
+++ src/app/signals/1-writable-signals/writable-signals.component.ts
@@ -1,11 +1,19 @@
-import { Component, ElementRef, ViewChild, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  computed,
+  signal,
+} from '@angular/core';
+import { codeLine } from '../../components-atom/component-atom.interface';
+import { CodeComponent } from '../../components-atom/code/code.component';
 
 @Component({
   selector: 'app-writable-signals',
   standalone: true,
-  imports: [],
   templateUrl: './writable-signals.component.html',
   styleUrl: './writable-signals.component.css',
+  imports: [CodeComponent],
 })
 export class WritableSignalsComponent {
   count = signal(0);
@@ -17,6 +25,19 @@ export class WritableSignalsComponent {
     this.count.update((value) => value + 1);
     console.log(this.count());
   }
+
+  lineas = computed<codeLine[]>(() => [
+    { line: 'count = signal(0);', active: true },
+    { line: 'setValue() {', active: false },
+    {
+      line: '  const inputValue = parseInt(this.myInput.nativeElement.value, 10);',
+      active: false,
+    },
+    { line: '  if (!isNaN(inputValue)) {', active: false },
+    { line: '    this.count.set(inputValue);', active: true }, // Esta línea se ejecuta solo si inputValue es un número
+    { line: '  }', active: false },
+    { line: '}', active: false },
+  ]);
   setValue() {
     const inputValue = parseInt(this.myInput.nativeElement.value, 10);
     if (!isNaN(inputValue)) {
