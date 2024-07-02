bdadd07cac810c229f25e2892e2a46c62cd64c43
Damian Sire <damiansire@gmail.com>
2024-07-02 12:04:11 -0300

    feat: add signal-equality example form

--- src/app/signals/signal-equality-functions/signal-equality-functions.component.ts
+++ src/app/signals/signal-equality-functions/signal-equality-functions.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { InputComponent } from '../../components-atom/input/input.component';
+import { ButtonComponent } from '../../components-atom/button/button.component';
 
 @Component({
   selector: 'app-signal-equality-functions',
   standalone: true,
-  imports: [],
   templateUrl: './signal-equality-functions.component.html',
-  styleUrl: './signal-equality-functions.component.css'
+  styleUrl: './signal-equality-functions.component.css',
+  imports: [InputComponent, ButtonComponent],
 })
 export class SignalEqualityFunctionsComponent {
-
+  data = signal(
+    { name: 'damian' },
+    {
+      equal: (prev, element) => {
+        return element.name === prev.name;
+      },
+    }
+  );
+  constructor() {
+    effect(() => {
+      console.log(this.data());
+    });
+  }
+  setValue() {
+    this.data.set({ name: 'pedro' });
+  }
 }
