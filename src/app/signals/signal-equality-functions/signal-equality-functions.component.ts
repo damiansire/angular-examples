404a50dab746839b4afb2067e577d49c0a181ab7
Damian Sire <damiansire@gmail.com>
2024-07-02 12:32:19 -0300

    feat: add variable box for show state of variable

--- src/app/signals/signal-equality-functions/signal-equality-functions.component.ts
+++ src/app/signals/signal-equality-functions/signal-equality-functions.component.ts
@@ -1,27 +1,28 @@
 import { Component, effect, signal } from '@angular/core';
 import { InputComponent } from '../../components-atom/input/input.component';
 import { ButtonComponent } from '../../components-atom/button/button.component';
+import { VariableBoxComponent } from '../../components-atom/variable-box/variable-box.component';
 
 @Component({
   selector: 'app-signal-equality-functions',
   standalone: true,
   templateUrl: './signal-equality-functions.component.html',
   styleUrl: './signal-equality-functions.component.css',
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent, VariableBoxComponent],
 })
 export class SignalEqualityFunctionsComponent {
-  name = signal('');
+  name = signal('dami');
   setName(event: string) {
     this.name.set(event);
   }
-  age = signal('');
+  age = signal('26');
   setAge(event: string) {
     this.age.set(event);
   }
 
-  person = signal({ name: '', age: '' });
-  setPerson(personName: string, personAge: string) {
-    const newData = { name: personName, age: personAge };
+  person = signal({ name: 'damian', surname: 'sire' });
+  setPerson(personName: string, personSurname: string) {
+    const newData = { name: personName, surname: personSurname };
     this.person.set(newData);
   }
 
