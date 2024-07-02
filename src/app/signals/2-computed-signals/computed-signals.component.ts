38b10d557c1f390520ae2490bb411203c74b7dd0
Damian Sire <damiansire@gmail.com>
2024-07-02 18:27:41 -0300

    feat: add code preview to computed signals

--- src/app/signals/2-computed-signals/computed-signals.component.ts
+++ src/app/signals/2-computed-signals/computed-signals.component.ts
@@ -7,13 +7,15 @@ import {
   signal,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CodeComponent } from '../../components-atom/code/code.component';
+import { codeLine } from '../../components-atom/component-atom.interface';
 
 @Component({
   selector: 'app-computed-signals',
   standalone: true,
-  imports: [CommonModule, FormsModule],
   templateUrl: './computed-signals.component.html',
   styleUrl: './computed-signals.component.css',
+  imports: [CommonModule, FormsModule, CodeComponent],
 })
 export class ComputedSignalsComponent {
   firstName: WritableSignal<string> = signal('Damian');
@@ -31,4 +33,21 @@ export class ComputedSignalsComponent {
     const value = eventTarget?.value || '';
     this.surname.set(value);
   }
+
+  lines = computed<codeLine[]>(() => [
+    {
+      line: 'firstName: WritableSignal<string> = signal("Damian");',
+      active: false,
+    },
+    {
+      line: 'surname: WritableSignal<string> = signal("Sire");',
+      active: false,
+    },
+    { line: 'fullName: Signal<string> = computed(() => {', active: false },
+    {
+      line: '  return `${this.firstName()} ${this.surname()}`;',
+      active: false,
+    }, // Se ejecuta cuando se crea `fullName`
+    { line: '});', active: false },
+  ]);
 }
