b26257d53e5e549cb73fdf6905a9404fefa06feb
Damian Sire <damiansire@gmail.com>
2024-07-02 09:53:52 -0300

    feat: add base logic for component destroy

--- src/app/signals/7-effect-destroy/component-destroy/component-destroy.component.ts
+++ src/app/signals/7-effect-destroy/component-destroy/component-destroy.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-component-destroy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './component-destroy.component.html',
-  styleUrl: './component-destroy.component.css'
+  styleUrl: './component-destroy.component.css',
 })
 export class ComponentDestroyComponent {
+  @Output() autoRefreshEvent = new EventEmitter<boolean>();
+  @Output() newIntervalOutput = new EventEmitter<Date>();
+  currentTime = new Date();
+  autoRefresh = signal(false);
+  intervalSave: any;
+  constructor() {
+    effect(() => {
+      if (this.autoRefresh()) {
+        this.intervalSave = setInterval(() => {
+          this.currentTime = new Date();
+          this.newIntervalOutput.emit(this.currentTime);
+        }, 1000);
+      } else {
+        clearInterval(this.intervalSave);
+      }
+    });
+  }
 
+  refreshTime() {
+    this.currentTime = new Date();
+  }
+  toggleAutoRefresh() {
+    this.autoRefreshEvent.emit(!this.autoRefresh());
+    this.autoRefresh.set(!this.autoRefresh());
+  }
 }
--- src/app/signals/7-effect-destroy/effect-destroy.component.ts
+++ src/app/signals/7-effect-destroy/effect-destroy.component.ts
@@ -1,10 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { ComponentDestroyComponent } from './component-destroy/component-destroy.component';
+import { EventHistoryComponent } from '../../components/event-history/event-history.component';
+import { CodeComponent } from '../../components-atom/code/code.component';
+import { codeLine } from '../../components-atom/component-atom.interface';
+import { HistoryElement } from '../../components/component.interface';
 
 @Component({
   selector: 'app-effect-destroy',
   standalone: true,
-  imports: [],
   templateUrl: './effect-destroy.component.html',
   styleUrl: './effect-destroy.component.css',
+  imports: [ComponentDestroyComponent, EventHistoryComponent, CodeComponent],
 })
-export class EffectDestroyComponent {}
+export class EffectDestroyComponent {
+  autoRefresh = signal(false);
+  appEventHistory = signal<HistoryElement[]>([]);
+  lines = computed<codeLine[]>(() => [
+    {
+      line: 'constructor() {',
+      active: false,
+    },
+    {
+      line: '  effect(() => {',
+      active: false,
+    },
+    {
+      line: '    if (this.autoRefresh()) {',
+      active: this.autoRefresh(),
+    },
+    {
+      line: '      this.intervalSave = setInterval(() => {',
+      active: this.autoRefresh(),
+    },
+    {
+      line: '        this.currentTime = new Date();',
+      active: this.autoRefresh(),
+    },
+    {
+      line: '        console.log("Estoy aca");',
+      active: this.autoRefresh(),
+    },
+    {
+      line: '      }, 1000);',
+      active: this.autoRefresh(),
+    },
+    {
+      line: '    } else {',
+      active: !this.autoRefresh(),
+    },
+    {
+      line: '      clearInterval(this.intervalSave);',
+      active: !this.autoRefresh(),
+    },
+    {
+      line: '    }',
+      active: false,
+    },
+    {
+      line: '  });',
+      active: false,
+    },
+    {
+      line: '}',
+      active: false,
+    },
+  ]);
+
+  showComponent = true;
+  destroy() {
+    this.showComponent = false;
+  }
+  setAutoRefresh(event: boolean) {
+    this.autoRefresh.set(event);
+  }
+  addConditionalCountRecomputation(
+    trigger: string,
+    newState: number | string,
+    isCountIncrement: boolean
+  ) {
+    this.appEventHistory.update((prevHistory) => {
+      const newHistory = prevHistory.length ? [...prevHistory] : [];
+      newHistory.push({
+        date: new Date(),
+        trigger,
+        newState,
+        isCountIncrement,
+      });
+      return newHistory;
+    });
+  }
+  getFormattedTime(date: Date) {
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  newIntervalOutput(event: Date) {
+    this.addConditionalCountRecomputation(
+      'interval',
+      this.getFormattedTime(event),
+      false
+    );
+  }
+}
