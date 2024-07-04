abcbf4dcf1474912b4dfc36844f02b22275d7391
Damian Sire <damiansire@gmail.com>
2024-07-04 02:06:30 -0300

    design: improve computed signals ui

--- src/app/signals/2-computed-signals/computed-signals.component.ts
+++ src/app/signals/2-computed-signals/computed-signals.component.ts
@@ -9,13 +9,14 @@ import {
 import { FormsModule } from '@angular/forms';
 import { CodeComponent } from '../../components-atom/code/code.component';
 import { codeLine } from '../../components-atom/component-atom.interface';
+import { TitleComponent } from '../../components-atom/title/title.component';
 
 @Component({
   selector: 'app-computed-signals',
   standalone: true,
   templateUrl: './computed-signals.component.html',
   styleUrl: './computed-signals.component.css',
-  imports: [CommonModule, FormsModule, CodeComponent],
+  imports: [CommonModule, FormsModule, CodeComponent, TitleComponent],
 })
 export class ComputedSignalsComponent {
   firstName: WritableSignal<string> = signal('Damian');
@@ -43,11 +44,11 @@ export class ComputedSignalsComponent {
       line: 'surname: WritableSignal<string> = signal("Sire");',
       active: false,
     },
-    { line: 'fullName: Signal<string> = computed(() => {', active: false },
+    { line: 'fullName: Signal<string> = computed(() => {', active: true },
     {
       line: '  return `${this.firstName()} ${this.surname()}`;',
-      active: false,
-    }, // Se ejecuta cuando se crea `fullName`
-    { line: '});', active: false },
+      active: true,
+    },
+    { line: '});', active: true },
   ]);
 }
