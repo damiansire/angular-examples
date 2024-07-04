44c2f85b0910b8752ab60f0257882adb5f72e623
Damian Sire <damiansire@gmail.com>
2024-07-04 02:01:54 -0300

    feat: add base structure for title component

---
+++ src/app/components-atom/title/title.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TitleComponent } from './title.component';
+
+describe('TitleComponent', () => {
+  let component: TitleComponent;
+  let fixture: ComponentFixture<TitleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TitleComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TitleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components-atom/title/title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './title.component.html',
+  styleUrl: './title.component.css'
+})
+export class TitleComponent {
+
+}
