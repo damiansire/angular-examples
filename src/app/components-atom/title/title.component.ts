5443757901d95069781c6be7eddda96ab23e0b98
Damian Sire <damiansire@gmail.com>
2024-07-04 02:06:17 -0300

    feat: add title component logic

--- src/app/components-atom/title/title.component.ts
+++ src/app/components-atom/title/title.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
   templateUrl: './title.component.html',
-  styleUrl: './title.component.css'
+  styleUrl: './title.component.css',
 })
 export class TitleComponent {
-
+  @Input() title: string = 'Missing Title';
 }
