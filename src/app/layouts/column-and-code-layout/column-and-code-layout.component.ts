f21a35fec5f23363b94b197fb3f8167a0fc162e5
Damian Sire <damiansire@gmail.com>
2024-07-05 20:03:22 -0300

    feat: colum-and-code-layout now have a code automatique

--- src/app/layouts/column-and-code-layout/column-and-code-layout.component.ts
+++ src/app/layouts/column-and-code-layout/column-and-code-layout.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal, Signal } from '@angular/core';
 import { TitleComponent } from '../../components-atom/title/title.component';
+import { codeLine } from '../../components-atom/component-atom.interface';
+import { CodeComponent } from '../../components-atom/code/code.component';
 
 @Component({
   selector: 'app-column-and-code-layout',
   standalone: true,
   templateUrl: './column-and-code-layout.component.html',
   styleUrl: './column-and-code-layout.component.css',
-  imports: [TitleComponent],
+  imports: [TitleComponent, CodeComponent],
 })
 export class ColumnAndCodeLayoutComponent {
   @Input() title = 'No Title';
+  @Input() codeLines: Signal<codeLine[]> = signal<codeLine[]>([]);
 }
