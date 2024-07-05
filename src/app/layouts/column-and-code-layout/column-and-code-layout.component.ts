0f977f4c3f4035d00d29a5e96dd158434bb4a754
Damian Sire <damiansire@gmail.com>
2024-07-05 17:36:05 -0300

    feat: add code for show the children in two columns

--- src/app/layouts/column-and-code-layout/column-and-code-layout.component.ts
+++ src/app/layouts/column-and-code-layout/column-and-code-layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../components-atom/title/title.component';
 
 @Component({
   selector: 'app-column-and-code-layout',
   standalone: true,
-  imports: [],
   templateUrl: './column-and-code-layout.component.html',
-  styleUrl: './column-and-code-layout.component.css'
+  styleUrl: './column-and-code-layout.component.css',
+  imports: [TitleComponent],
 })
-export class ColumnAndCodeLayoutComponent {
-
-}
+export class ColumnAndCodeLayoutComponent {}
