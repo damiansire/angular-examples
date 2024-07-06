c029fa5080ee7776e870289be200a6b7a3cfcf96
Damian Sire <damiansire@gmail.com>
2024-07-06 02:54:28 -0300

    refactor: create base component for sidebar menu

---
+++ src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-menu.component.html',
+  styleUrl: './sidebar-menu.component.css'
+})
+export class SidebarMenuComponent {
+
+}
