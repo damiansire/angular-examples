75b7f5e8a05f0aa8c3d585ecebb398dc19e96468
Damian Sire <damiansire@gmail.com>
2024-07-06 02:43:31 -0300

    feat: add flat component structure for computed-signals level

---
+++ src/app/signals/level-2-computed-signals/computed-signals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-computed-signals',
+  standalone: true,
+  imports: [],
+  templateUrl: './computed-signals.component.html',
+  styleUrl: './computed-signals.component.css'
+})
+export class ComputedSignalsComponent {
+
+}
