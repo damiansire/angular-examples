14697b8efac97e3306b52fa8753cf2569baedace
Damian Sire <damiansire@gmail.com>
2024-07-09 18:29:07 -0300

    feat: Menu color now depends on levelState

--- src/app/components/sidebar-menu/components/menu-option/menu-option.component.ts
+++ src/app/components/sidebar-menu/components/menu-option/menu-option.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CustomRoute } from '../../../../app.routes';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+type LevelState = 'pending' | 'currentLevel' | 'win';
 
 @Component({
   selector: 'app-menu-option',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './menu-option.component.html',
   styleUrl: './menu-option.component.css',
 })
@@ -16,4 +19,5 @@ export class MenuOptionComponent {
     id: '',
     subLevels: [],
   };
+  @Input() levelState: LevelState = 'pending';
 }
