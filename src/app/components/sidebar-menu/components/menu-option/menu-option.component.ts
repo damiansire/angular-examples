8a8ecd824348a4bae21c3bf22478423e42d4a604
Damian Sire <damiansire@gmail.com>
2024-07-09 17:20:45 -0300

    feat: create functional menu option component for sidebar

--- src/app/components/sidebar-menu/components/menu-option/menu-option.component.ts
+++ src/app/components/sidebar-menu/components/menu-option/menu-option.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CustomRoute } from '../../../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu-option',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './menu-option.component.html',
-  styleUrl: './menu-option.component.css'
+  styleUrl: './menu-option.component.css',
 })
 export class MenuOptionComponent {
-
+  @Input({ required: true }) menuItem: CustomRoute = {
+    path: '',
+    component: undefined,
+    id: '',
+    subLevels: [],
+  };
 }
--- src/app/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { menuItems } from '../../app.routes';
+import { CustomRoute, menuItems } from '../../app.routes';
+import { MenuOptionComponent } from './components/menu-option/menu-option.component';
 
 @Component({
   selector: 'app-sidebar-menu',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuOptionComponent],
   templateUrl: './sidebar-menu.component.html',
   styleUrl: './sidebar-menu.component.css',
 })
 export class SidebarMenuComponent {
-  menuItems = menuItems;
+  menuItems: CustomRoute[] = menuItems;
 
   constructor() {
     console.log(this.menuItems);
