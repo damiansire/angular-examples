67bd0ee3566f0f8574f0089f764f746e5f4964e0
Damian Sire <damiansire@gmail.com>
2024-07-10 03:37:57 -0300

    feat: add menu sub level option component

--- src/app/components/sidebar-menu/components/menu-sub-level-option/menu-sub-level-option.component.ts
+++ src/app/components/sidebar-menu/components/menu-sub-level-option/menu-sub-level-option.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CustomRoute } from '../../../../app.routes';
+import { LevelState } from '../../../component.interface';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu-sub-level-option',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './menu-sub-level-option.component.html',
-  styleUrl: './menu-sub-level-option.component.css'
+  styleUrl: './menu-sub-level-option.component.css',
 })
 export class MenuSubLevelOptionComponent {
+  @Input() item: CustomRoute = {
+    path: '',
+    component: undefined,
+    id: '',
+    subLevels: [],
+  };
 
+  @Input() levelState: LevelState = 'pending';
 }
