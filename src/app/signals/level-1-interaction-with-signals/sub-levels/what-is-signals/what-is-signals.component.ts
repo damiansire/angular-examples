a1b3d74f621b37fc574199dbf0460e38b028d99b
Damian Sire <damiansire@gmail.com>
2024-07-13 12:43:12 -0300

    feat: add general data for template

--- src/app/signals/level-1-interaction-with-signals/sub-levels/what-is-signals/what-is-signals.component.ts
+++ src/app/signals/level-1-interaction-with-signals/sub-levels/what-is-signals/what-is-signals.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../../../components-atom/title/title.component';
 
 @Component({
   selector: 'app-what-is-signals',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './what-is-signals.component.html',
-  styleUrl: './what-is-signals.component.css'
+  styleUrl: './what-is-signals.component.css',
 })
 export class WhatIsSignalsComponent {
-
+  title = 'What are signals?';
 }
