2004a7fe0dd76df02d486b01dfb065df28cded77
Damian Sire <damiansire@gmail.com>
2024-07-26 19:01:01 -0300

    feat: add base structure for introduction level

---
+++ src/app/signals/level-0-introduction/introduction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introduction',
+  standalone: true,
+  imports: [],
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.css'
+})
+export class IntroductionComponent {
+
+}
