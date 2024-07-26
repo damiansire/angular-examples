663d6a991dbd1f8786f1c7314b0031aeaeb75a0d
Damian Sire <damiansire@gmail.com>
2024-07-26 20:23:23 -0300

    feat: add page for show diferent data types

--- src/app/components-draw/container-variable-box-draw/container-variable-box-draw.component.ts
+++ src/app/components-draw/container-variable-box-draw/container-variable-box-draw.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { VariableBoxDrawComponent } from '../variable-box-draw/variable-box-draw.component';
 
 @Component({
   selector: 'app-container-variable-box-draw',
   standalone: true,
-  imports: [],
+  imports: [VariableBoxDrawComponent],
   templateUrl: './container-variable-box-draw.component.html',
-  styleUrl: './container-variable-box-draw.component.css'
+  styleUrl: './container-variable-box-draw.component.css',
 })
 export class ContainerVariableBoxDrawComponent {
-
+  @Input() variableName: string = '';
 }
--- src/app/signals/level-0-introduction/sub-levels/variables/variables.component.ts
+++ src/app/signals/level-0-introduction/sub-levels/variables/variables.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { VariableBoxDrawComponent } from '../../../../components-draw/variable-box-draw/variable-box-draw.component';
 
 @Component({
   selector: 'app-variables',
   standalone: true,
-  imports: [],
+  imports: [VariableBoxDrawComponent],
   templateUrl: './variables.component.html',
-  styleUrl: './variables.component.css'
+  styleUrl: './variables.component.css',
 })
 export class VariablesComponent {
-
+  dataTypes = ['Number', 'String', 'Boolean', 'Object', 'Array'];
 }
