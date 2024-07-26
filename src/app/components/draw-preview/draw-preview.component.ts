efefee6b8cc6828be09dcfb1f97c48bd3616e895
Damian Sire <damiansire@gmail.com>
2024-07-26 18:57:17 -0300

    feat: add base structure for use images

--- src/app/components/draw-preview/draw-preview.component.ts
+++ src/app/components/draw-preview/draw-preview.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-draw-preview',
   standalone: true,
   imports: [],
   templateUrl: './draw-preview.component.html',
-  styleUrl: './draw-preview.component.css'
+  styleUrl: './draw-preview.component.css',
 })
 export class DrawPreviewComponent {
-
+  @Input() imgUrl: string = '';
 }
--- src/app/signals/level-1-interaction-with-signals/sub-levels/what-is-signals/what-is-signals.component.ts
+++ src/app/signals/level-1-interaction-with-signals/sub-levels/what-is-signals/what-is-signals.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../../../../components-atom/title/title.component';
 import { WrapperAnimationComponent } from './wrapper-animation/wrapper-animation.component';
+import { DrawPreviewComponent } from '../../../../components/draw-preview/draw-preview.component';
 
 @Component({
   selector: 'app-what-is-signals',
   standalone: true,
-  imports: [TitleComponent, WrapperAnimationComponent],
+  imports: [TitleComponent, WrapperAnimationComponent, DrawPreviewComponent],
   templateUrl: './what-is-signals.component.html',
   styleUrl: './what-is-signals.component.css',
 })
