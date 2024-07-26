e95527884b47f9fc3e2a800e8777a8fdcdf629d3
Damian Sire <damiansire@gmail.com>
2024-07-26 19:29:09 -0300

    feat: now variable box can change the text

--- src/app/components-draw/variable-box-draw/variable-box-draw.component.ts
+++ src/app/components-draw/variable-box-draw/variable-box-draw.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-variable-box-draw',
   standalone: true,
   imports: [],
   templateUrl: './variable-box-draw.component.html',
-  styleUrl: './variable-box-draw.component.css'
+  styleUrl: './variable-box-draw.component.css',
 })
 export class VariableBoxDrawComponent {
-
+  @Input() variableName: string = '';
 }
