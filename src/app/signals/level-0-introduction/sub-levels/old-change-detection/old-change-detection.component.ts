9b2dac7eee2009ecd6ad462d78515a723a06d069
Damian Sire <damiansire@gmail.com>
2024-07-27 09:54:43 -0300

    feat(old-change-detection): add button for next step and prev step

--- src/app/signals/level-0-introduction/sub-levels/old-change-detection/old-change-detection.component.ts
+++ src/app/signals/level-0-introduction/sub-levels/old-change-detection/old-change-detection.component.ts
@@ -13,49 +13,62 @@ import { NodeTreeComponent } from '../../../../components-draw/node-tree/node-tr
   styleUrl: './old-change-detection.component.css',
 })
 export class OldChangeDetectionComponent {
-  step = 2;
+  step = signal(0);
+
+  incrementStep() {
+    if (this.step() < 3) {
+      this.step.update((currentStep) => currentStep + 1);
+    }
+  }
+
+  decrementStep() {
+    if (this.step() > 0) {
+      this.step.update((currentStep) => currentStep - 1);
+    }
+  }
+
   data = signal<NodeTree[]>([
     {
       name: 'main',
       x: 550,
       y: 100,
-      color: this.step > 0 ? '#90EE90' : '',
+      color: this.step() > 0 ? '#90EE90' : '',
     },
     {
       name: 'article',
       x: 700,
       y: 300,
-      color: this.step > 1 ? '#90EE90' : '',
+      color: this.step() > 1 ? '#90EE90' : '',
     },
     {
       name: 'section',
       x: 400,
       y: 300,
-      color: this.step > 1 ? '#90EE90' : '',
+      color: this.step() > 1 ? '#90EE90' : '',
     },
     {
       name: 'h2',
       x: 300,
       y: 500,
-      color: this.step > 2 ? '#90EE90' : '',
+      color: this.step() > 2 ? '#90EE90' : '',
     },
     {
       name: 'p',
       x: 500,
       y: 500,
-      color: this.step > 2 ? '#90EE90' : '',
+      color: this.step() > 2 ? '#90EE90' : '',
     },
     {
       name: 'h3',
       x: 600,
       y: 500,
-      color: this.step > 2 ? '#90EE90' : '',
+      color: this.step() > 2 ? '#90EE90' : '',
     },
     {
       name: 'p2',
       x: 800,
       y: 500,
-      color: this.step > 2 ? '#90EE90' : '',
+      color: this.step() > 2 ? '#90EE90' : '',
     },
   ]);
   links = signal<Link[]>([
