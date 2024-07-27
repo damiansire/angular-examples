9093e9e616b94eca08ce736f59e6211e56af0105
Damian Sire <damiansire@gmail.com>
2024-07-27 09:50:04 -0300

    feat: add old change detection example

--- src/app/signals/level-0-introduction/sub-levels/old-change-detection/old-change-detection.component.ts
+++ src/app/signals/level-0-introduction/sub-levels/old-change-detection/old-change-detection.component.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {
+  NodeTree,
+  Link,
+} from '../../../../components-draw/components-draw.inferface';
+import { NodeTreeComponent } from '../../../../components-draw/node-tree/node-tree.component';
 
 @Component({
   selector: 'app-old-change-detection',
   standalone: true,
-  imports: [],
+  imports: [NodeTreeComponent],
   templateUrl: './old-change-detection.component.html',
-  styleUrl: './old-change-detection.component.css'
+  styleUrl: './old-change-detection.component.css',
 })
 export class OldChangeDetectionComponent {
-
+  step = 2;
+  data = signal<NodeTree[]>([
+    {
+      name: 'main',
+      x: 550,
+      y: 100,
+      color: this.step > 0 ? '#90EE90' : '',
+    },
+    {
+      name: 'article',
+      x: 700,
+      y: 300,
+      color: this.step > 1 ? '#90EE90' : '',
+    },
+    {
+      name: 'section',
+      x: 400,
+      y: 300,
+      color: this.step > 1 ? '#90EE90' : '',
+    },
+    {
+      name: 'h2',
+      x: 300,
+      y: 500,
+      color: this.step > 2 ? '#90EE90' : '',
+    },
+    {
+      name: 'p',
+      x: 500,
+      y: 500,
+      color: this.step > 2 ? '#90EE90' : '',
+    },
+    {
+      name: 'h3',
+      x: 600,
+      y: 500,
+      color: this.step > 2 ? '#90EE90' : '',
+    },
+    {
+      name: 'p2',
+      x: 800,
+      y: 500,
+      color: this.step > 2 ? '#90EE90' : '',
+    },
+  ]);
+  links = signal<Link[]>([
+    {
+      source: 'main',
+      target: 'article',
+    },
+    {
+      source: 'main',
+      target: 'section',
+    },
+    {
+      source: 'section',
+      target: 'h2',
+    },
+    {
+      source: 'section',
+      target: 'p',
+    },
+    {
+      source: 'article',
+      target: 'h3',
+    },
+    {
+      source: 'article',
+      target: 'p2',
+    },
+  ]);
 }
