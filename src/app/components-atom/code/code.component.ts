77b895d8a8fe6aad79bd7eec590f5a203d0a802a
Damian Sire <damiansire@gmail.com>
2024-07-27 09:04:38 -0300

    feat: now text size can change the size of letter from prop

--- src/app/components-atom/code/code.component.ts
+++ src/app/components-atom/code/code.component.ts
@@ -2,6 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, Signal, signal } from '@angular/core';
 import { CodeLine } from '../component-atom.interface';
 
+type TailwindTextSize =
+  | 'text-xs'
+  | 'text-sm'
+  | 'text-base'
+  | 'text-lg'
+  | 'text-xl'
+  | 'text-2xl'
+  | 'text-3xl'
+  | 'text-4xl'
+  | 'text-5xl'
+  | 'text-6xl'
+  | 'text-7xl'
+  | 'text-8xl'
+  | 'text-9xl';
+
 @Component({
   selector: 'app-code',
   standalone: true,
@@ -10,5 +25,6 @@ import { CodeLine } from '../component-atom.interface';
   styleUrl: './code.component.css',
 })
 export class CodeComponent {
+  @Input() textSize: TailwindTextSize = 'text-2xl';
   @Input() lines: Signal<CodeLine[]> = signal([]);
 }
