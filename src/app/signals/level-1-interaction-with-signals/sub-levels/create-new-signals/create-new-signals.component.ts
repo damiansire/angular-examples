24ae10847e43f0518722fc5e1980c86aa0cc2a3d
Damian Sire <damiansire@gmail.com>
2024-07-27 11:38:54 -0300

    feat: add getter function example

--- src/app/signals/level-1-interaction-with-signals/sub-levels/create-new-signals/create-new-signals.component.ts
+++ src/app/signals/level-1-interaction-with-signals/sub-levels/create-new-signals/create-new-signals.component.ts
@@ -1,19 +1,25 @@
 import { Component, computed } from '@angular/core';
 import { ColumnAndCodeLayoutComponent } from '../../../../layouts/column-and-code-layout/column-and-code-layout.component';
 import { CodeLine } from '../../../../components-atom/component-atom.interface';
+import { TitleComponent } from '../../../../components-atom/title/title.component';
+import { CodeComponent } from '../../../../components-atom/code/code.component';
 
 @Component({
   selector: 'app-create-new-signals',
   standalone: true,
-  imports: [ColumnAndCodeLayoutComponent],
+  imports: [ColumnAndCodeLayoutComponent, TitleComponent, CodeComponent],
   templateUrl: './create-new-signals.component.html',
   styleUrl: './create-new-signals.component.css',
 })
 export class CreateNewSignalsComponent {
   lines = computed<CodeLine[]>(() => [
     { line: 'count = signal(0);', active: true },
-    { line: 'setValue() {', active: false },
-    { line: '    this.count.set(inputValue);', active: true }, // Esta línea se ejecuta solo si inputValue es un número
-    { line: '}', active: false },
+  ]);
+  htmlLines = computed<CodeLine[]>(() => [
+    { line: '<div>', active: false },
+    { line: '     <span>', active: false },
+    { line: '            count()', active: true },
+    { line: '     </span>', active: false },
+    { line: '<div>', active: false },
   ]);
 }
