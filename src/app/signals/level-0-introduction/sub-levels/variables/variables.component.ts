57c637da28d78549fb478743f8a803e1c3ed6aa5
Damian Sire <damiansire@gmail.com>
2024-07-26 21:27:04 -0300

    feat: handle multiples examples in the same screen

--- src/app/signals/level-0-introduction/sub-levels/variables/variables.component.ts
+++ src/app/signals/level-0-introduction/sub-levels/variables/variables.component.ts
@@ -3,6 +3,11 @@ import { VariableBoxDrawComponent } from '../../../../components-draw/variable-b
 import { CodeLine } from '../../../../components-atom/component-atom.interface';
 import { CodeComponent } from '../../../../components-atom/code/code.component';
 
+interface DataTypeExample {
+  name: string;
+  examples: string[];
+}
+
 @Component({
   selector: 'app-variables',
   standalone: true,
@@ -11,15 +16,60 @@ import { CodeComponent } from '../../../../components-atom/code/code.component';
   styleUrl: './variables.component.css',
 })
 export class VariablesComponent {
-  dataTypes = ['Number', 'String', 'Boolean', 'Object', 'Array'];
-  numberValues = ['1', '6', '56', '5.4', '10.1'];
-  selectedValue = signal('1');
+  dataTypes = ['Number'];
+  typesExamples: { [key: string]: DataTypeExample } = {
+    Number: {
+      name: 'Number',
+      examples: ['1', '6', '56', '5.4', '10.1'],
+    },
+    String: {
+      name: 'String',
+      examples: ['A', 'B', 'C', 'Hello', 'Count$'],
+    },
+    Boolean: {
+      name: 'Boolean',
+      examples: ['True', 'False'],
+    },
+    Object: {
+      name: 'Object',
+      examples: [
+        '{}',
+        '{name: "Damian"}',
+        '{name: "Damian", surname: "Sire", age : 26}',
+      ],
+    },
+    Array: {
+      name: 'Array',
+      examples: [
+        '[]',
+        '[1,2,3,4,5]',
+        '[true, false]',
+        '[ 1 , 2 , true , {name : "damian"} ]',
+      ],
+    },
+  };
+  selectedType = signal(this.typesExamples['Number']);
+  selectedExampleValue = signal(this.selectedType().examples[0]);
   lines = computed<CodeLine[]>(() => [
-    { line: `var value = ${this.selectedValue()}`, active: false },
-    { line: `const value = ${this.selectedValue()}`, active: false },
-    { line: `let value = ${this.selectedValue()}`, active: false },
+    { line: `var value = ${this.selectedExampleValue()}`, active: false },
+    {
+      line: `const value = ${this.selectedExampleValue()}`,
+      active: false,
+    },
+    { line: `let value = ${this.selectedExampleValue()}`, active: false },
   ]);
-  setValue(value: any) {
-    this.selectedValue.set(value.name);
+
+  ngOnInit() {
+    this.dataTypes = Object.keys(this.typesExamples);
+  }
+
+  selectExample(value: any) {
+    this.selectedExampleValue.set(value.name);
+  }
+
+  selectType(value: any) {
+    const variableName: string = value.name;
+    const newSelectedType = this.typesExamples[variableName];
+    this.selectedType.set(newSelectedType);
   }
 }
