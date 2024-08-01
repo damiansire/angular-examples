dee1fa1afcd1dcac232c0e9c41a5c3bfc9803b72
Damian Sire <damiansire@gmail.com>
2024-07-31 21:29:30 -0300

    structure: add base structure for tree

---
+++ src/app/components/tree/tree.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tree',
+  standalone: true,
+  imports: [],
+  templateUrl: './tree.component.html',
+  styleUrl: './tree.component.css'
+})
+export class TreeComponent {
+
+}
