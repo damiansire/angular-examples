93db22e6eacde3b0ee5e3f66d0b7d908042ff702
Damian Sire <damiansire@gmail.com>
2024-08-02 18:20:49 -0300

    feat(title-component): add dinamic textSize props

--- src/app/components-atom/title/title.component.ts
+++ src/app/components-atom/title/title.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
-
+import { TailwindTextSize } from '../../interfaces/tailwind-css.interface';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './title.component.html',
   styleUrl: './title.component.css',
 })
 export class TitleComponent {
   @Input() title: string = 'Missing Title';
+  @Input() textSize: TailwindTextSize = 'text-4xl';
 }
