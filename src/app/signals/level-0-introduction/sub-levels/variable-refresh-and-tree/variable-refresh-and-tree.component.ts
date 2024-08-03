0ee24ef63268100f6cdba12dfc88c4236205ba39
Damian Sire <damiansire@gmail.com>
2024-08-03 16:21:41 -0300

    feat: add dinamic tree contruction in variable refresh

--- src/app/signals/level-0-introduction/sub-levels/variable-refresh-and-tree/variable-refresh-and-tree.component.ts
+++ src/app/signals/level-0-introduction/sub-levels/variable-refresh-and-tree/variable-refresh-and-tree.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TitleComponent } from '../../../../components-atom/title/title.component';
 import { CodeComponent } from '../../../../components-atom/code/code.component';
 import { TreeComponent } from '../../../../components/tree/tree.component';
 import { CountIncrementComponent } from './count-increment/count-increment.component';
 import { ButtonComponent } from '../../../../components-atom/button/button.component';
+import { CodeClick } from '../../../../components-atom/code/code.interface';
 
 @Component({
   selector: 'app-variable-refresh-and-tree',
@@ -45,6 +46,7 @@ export class VariableRefreshAndTreeComponent {
     <p> Multiplo de 3: {{ count % 3 ? "No" : "Si" }} </p>
   </section>
 </section>  `;
+  nodesToShow = signal<string[]>([]);
   sliderNumber = 0;
   nextSlider() {
     this.sliderNumber++;
@@ -55,5 +57,20 @@ export class VariableRefreshAndTreeComponent {
     if (this.sliderNumber === -1) {
       this.sliderNumber = 3;
     }
+  }
+  codeClickHandler(event: CodeClick) {
+    if (event.action == 'Select') {
+      this.addNode(event.id);
+    } else if (event.action == 'Deselect') {
+      this.removeNode(event.id);
+    }
+  }
+  addNode(id: string) {
+    this.nodesToShow.update((currentNodes) => [...currentNodes, id]);
+  }
+  removeNode(id: string) {
+    this.nodesToShow.update((currentNodes) =>
+      currentNodes.filter((node) => node !== id)
+    );
   }
 }
